1c7593206a936007f5f7cacb04c7271d3fa95bff
MaksymKanaev <kanaev.m.i@gmail.com>
2023-06-26 21:47:45 +0300

    create Tower

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let footer = document.getElementById('game__footer');
 let magazin = document.getElementById('game__magazin');
+let magazinElement = document.querySelector('#game__magazin span');
 let towerSkin = 'skin1';
 let board = document.getElementById('game__board');
 let field = document.getElementById('game__field');
