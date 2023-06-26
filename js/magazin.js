bfeeeabe09df34872f4f0acff58d30222af79a0a
MaksymKanaev <kanaev.m.i@gmail.com>
2023-06-26 15:56:51 +0300

    magazin

---
+++ js/magazin.js
@@ -0,0 +1,17 @@
+
+// получаем список элементов с классом skin
+const skins = document.querySelectorAll('.skin');
+
+// проходимся циклом по всем элементам и добавляем обработчик событий на клик
+skins.forEach(skin => {
+   skin.addEventListener('click', () => {
+      // получаем значение атрибута data-skin кликнутого элемента
+      const skinValue = skin.dataset.skin;
+      // устанавливаем значение gamerSkin равным выбранному значению
+      gamerSkin = skinValue;
+      // проходимся по всем элементам и удаляем класс selected
+      skins.forEach(s => s.classList.remove('selected'));
+      // добавляем класс selected выбранному элементу
+      skin.classList.add('selected');
+   });
+});
