f032c1e8d7d51993437d9e4487ff3555bfed9f4b
NATA <118177362+LifeLong-LeArneR17@users.noreply.github.com>
2023-06-27 22:07:08 +0300

    fix
    fix

--- js/enemy.js
+++ js/enemy.js
@@ -4,31 +4,29 @@ let enemy; // Глобальная переменная enemy
 function createEnemy() {
     enemy = document.createElement("div"); 
     enemy.className = "enemy skin" + random(1, 3);
-    enemy.style.top = calculateTopPosition() + "px";
+    enemy.style.top = calculateTopPosition()  + "px";
     enemy.style.left = 1227 + "px";
     appEnemy.appendChild(enemy);
     MoveEnemy(enemy);
 }
 
 function calculateTopPosition() {
-    const gridHeight = 120; // Высота каждой ячейки в сетке
-    const characterHeight = 120/* Ваша высота персонажа */; // Высота персонажа
-    const gameFieldHeight = document.querySelector("#game__field").offsetHeight;
-    const cellHeight = gridHeight + 0.5; // Высота ячейки с учетом промежутка между ними
-
-    // Вычисляем максимальное количество ячеек по вертикали
-    const maxCells = Math.floor(gameFieldHeight / cellHeight);
-
-    // Вычисляем максимальное смещение по вертикали, чтобы оставаться в пределах игрового поля
-    const maxOffset = (maxCells - 1) * cellHeight - characterHeight;
-
-    // Случайное смещение от 0 до максимального смещения
-    const offset = random(0, maxOffset);
-
-    // Выравнивание по центру ячейки с учетом высоты персонажа
-    const top = offset + (cellHeight - gridHeight) / 2 + characterHeight / 2;
-
-    return top;
+   let top = random(1,5);
+   if(top == 1) {
+    return 137;
+   };
+   if (top == 2) {
+  return 259;
+   }
+   if(top == 3) {
+    return 384;
+   }
+   if(top == 4) {
+    return 505;
+   }
+   if(top == 5) {
+    return 628;
+   }
 }
 
 
